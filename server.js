e27f398c29d965e4e1a9d8575be257d58978d126
CLEITON DA SILVA BATISTA <232.976493@universidadesenaicimatec.edu.br>
2026-03-31 15:30:45 -0300

    Criando Rota para cadastro.html

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
 const pacientesRoutes = require('./routes/pacientes')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// servir arquivos da pasta public
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'cadastro.html'))
+})
 
 app.use(pacientesRoutes)
 
