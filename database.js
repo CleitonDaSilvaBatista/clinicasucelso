386d22512efa91c8407e8d5b5689df043cc48ca8
CLEITON DA SILVA BATISTA <232.976493@universidadesenaicimatec.edu.br>
2026-03-31 16:49:39 -0300

    Ultimo commit

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js')
 
 const supabase = createClient(
-'https://jmkyimzpvklzmdcnbgax.supabase.co',
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impta3lpbXpwdmtsem1kY25iZ2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NzY2OTUsImV4cCI6MjA5MDU1MjY5NX0.DfofAqC6rouglVPjxvgR0_LbDM6a1DPt3Rg_zkQFZJ0'
+    'https://jmkyimzpvklzmdcnbgax.supabase.co', /* URL do seu projeto Supabase */
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impta3lpbXpwdmtsem1kY25iZ2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NzY2OTUsImV4cCI6MjA5MDU1MjY5NX0.DfofAqC6rouglVPjxvgR0_LbDM6a1DPt3Rg_zkQFZJ0'/* Chave de API do seu projeto Supabase */
 )
 
 module.exports = supabase
